cd83d91bc34be0bcee0e843e0d3b6970ca81e4e9
YoungChan Kim <kyc4421194@gmail.com>
2021-03-03 22:51:22 +0900

    commit before merge

---
+++ build/CMakeFiles/geometry_class.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/geometry_class.dir/main.cpp.o"
+  "geometry_class"
+  "geometry_class.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/geometry_class.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cody/Downloads/cpp_practice/geometry_class
+# Build directory: /home/cody/Downloads/cpp_practice/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
